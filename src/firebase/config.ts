2752c3e7f175e128f3090862f41a219b5787edee
Nicat Hüseynli <huseynlinicat2010@gmail.com>
2025-11-24 13:35:09 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
-import { FirebaseOptions } from 'firebase/app';
-
-export const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+export const firebaseConfig = {
+  "projectId": "studio-5613091736-d0bd4",
+  "appId": "1:263062715266:web:4acd4d5d12778a4ff02d9b",
+  "apiKey": "AIzaSyCraxggUmqXOuIzUnm1hm77m_1qW-28ERo",
+  "authDomain": "studio-5613091736-d0bd4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "263062715266"
 };
